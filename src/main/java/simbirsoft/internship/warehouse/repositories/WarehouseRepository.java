d6885dc5077dd404ede089c5847f318c2f2629bd
vital <vitalik.sahnov@yandex.ru>
2021-03-18 17:13:40 +0400

    Changed the warehouse repository.

--- src/main/java/simbirsoft/internship/warehouse/repositories/WarehouseRepository.java
+++ src/main/java/simbirsoft/internship/warehouse/repositories/WarehouseRepository.java
@@ -2,8 +2,10 @@ package simbirsoft.internship.warehouse.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import simbirsoft.internship.warehouse.entities.Product;
 import simbirsoft.internship.warehouse.entities.Warehouse;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+    Warehouse getByProduct(Product product);
 }
