ae8681743d1e7d0bf0232a6e1aa4da32502c8410
vital <vitalik.sahnov@yandex.ru>
2021-03-25 02:28:31 +0400

    Added new dto.

---
+++ src/main/java/simbirsoft/internship/warehouse/dto/PurchaseDto.java
@@ -0,0 +1,24 @@
+package simbirsoft.internship.warehouse.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ApiModel(value = "model purchase")
+public class PurchaseDto {
+    private Long id;
+
+    @ApiModelProperty(value = "purchase map, key - product id, value - product quantity", example = "1, 1254")
+    private HashMap<Long, Long> products;
+
+    @ApiModelProperty(value = "purchase date", example = "2021.12.20")
+    private Date orderDate;
+}
