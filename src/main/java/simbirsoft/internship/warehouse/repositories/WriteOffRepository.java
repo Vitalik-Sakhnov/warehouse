5eef49288fd2959df09d7e34d3fbb69e9392fea7
vital <vitalik.sahnov@yandex.ru>
2021-04-01 18:22:57 +0400

    Changed write off repository and controller.

--- src/main/java/simbirsoft/internship/warehouse/repositories/WriteOffRepository.java
+++ src/main/java/simbirsoft/internship/warehouse/repositories/WriteOffRepository.java
@@ -1,9 +1,17 @@
 package simbirsoft.internship.warehouse.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import simbirsoft.internship.warehouse.entities.WriteOff;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface WriteOffRepository extends JpaRepository<WriteOff, Long> {
+
+    @Query("SELECT w FROM WriteOff w WHERE w.isApproved=true AND w.writeOffDate BETWEEN :firstDate AND :secondDate")
+    List<WriteOff> periodOfTime(@Param("firstDate") Date firstDate, @Param("secondDate")Date secondDate);
 }
